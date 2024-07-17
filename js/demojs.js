d2850041ce752cbc199e413357eca6ed30f02b13
Anhkhanh123456789 <roster1221@gmail.com>
2024-07-17 22:21:54 +0700

    Nop Bai Tap

---
+++ js/demojs.js
@@ -0,0 +1,14 @@
+function songuyento(n){
+    if(n==1)
+    {
+        return false;
+    }
+    for(var i=2; i<Math.sqrt(n);i++)
+    {
+        if(n%i==0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
